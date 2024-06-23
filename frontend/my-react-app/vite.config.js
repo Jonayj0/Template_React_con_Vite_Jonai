cdb549ebecb64178eb2e4168f15f8e59028ec115
Jonai <jonaybolanosmolina@gmail.com>
2024-06-23 21:22:38 +0100

    Plantilla añadida configuracion a vite.config

--- frontend/my-react-app/vite.config.js
+++ frontend/my-react-app/vite.config.js
@@ -5,3 +5,22 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
+
+
+// -------------------------------------CONFIGURACIÓN AVANZADA PARA PROYECTOS MÁS COMPLEJOS----------------------------//
+// --------------------------------------------------------------------------------------------------------------------//
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// // Configuración extendida de Vite para una aplicación React
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     port: 5173,
+//   },
+//   resolve: {
+//     alias: {
+//       '@': '/src',
+//     },
+//   },
+// });
